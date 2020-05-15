f32214bc59e7325eeaaeccdae954eff60e05e88d
Robert Sebastian Herlim <ngiooong@yahoo.com>
2020-05-15 14:54:47 +0900

    wip: refactor code but still stuck at python packaging problem

--- poc/poc.py
+++ poc/poc.py
@@ -1,14 +1,7 @@
-from concurrent.futures import _base
-from concurrent.futures.thread import ThreadPoolExecutor
-from threading import Thread
-from time import sleep
-from typing import Iterable
-
-from flask import Flask, Response, current_app
 from requests import get as http_get
 
-from bootstrap import IStartable
-from node import INode, ResponseLevel, inverse_response_level
+from node import INode, ResponseLevel
+from poc.bootstrap_flask import POCNodeStartable
 
 
 class POCNode(INode):
@@ -38,71 +31,6 @@ class POCNode(INode):
         level = response_level.value
         response = http_get(self.get_url(f'/response/{level}'))
         return response.status_code == 200
-
-
-def create_application(app_name, dependencies=None):
-    dependencies = [] if dependencies is None else dependencies
-
-    app = Flask(app_name)
-    app.__response_level = ResponseLevel.NORMAL
-    app.__dependencies = dependencies
-    app.__executor = ThreadPoolExecutor(max_workers=16)
-
-    # check dependencies must run asynchronously
-    def check_dependencies(dependencies: Iterable[str], executor: _base.Executor) -> bool:
-        futures = []
-        for target in dependencies:
-            future = executor.submit(lambda: http_get(f'http://{target}/'))
-            futures.append(future)
-
-        for future in futures:
-            result = future.result()
-            is_ok = result.status_code == 200
-            if not is_ok:
-                return False
-
-        return True
-
-    @app.route('/', methods=['GET'])
-    @app.route('/ping', methods=['GET'])
-    def ping():
-        response_level = current_app.__response_level
-        if response_level == ResponseLevel.NORMAL:
-            dependencies_ok = check_dependencies(current_app.__dependencies, current_app.__executor)
-            message, code = ('OK', 200) if dependencies_ok else ('UNHEALTHY', 500)
-            return Response(message, code)
-
-        elif response_level == ResponseLevel.TERMINATED:
-            return Response('UNHEALTHY', 500)
-
-        else:
-            sleep(5.0)
-            dependencies_ok = check_dependencies(current_app.__dependencies, current_app.__executor)
-            message, code = ('OK', 200) if dependencies_ok else ('UNHEALTHY', 500)
-            return Response(message, code)
-
-    @app.route('/response/<level>', methods=['GET'])
-    def set_response_level(level):
-        try:
-            level = int(level)
-            response_level = inverse_response_level(level)
-            current_app.__response_level = response_level
-            return 'OK'
-        except ValueError as e:
-            return str(e)
-
-    return app
-
-
-class POCNodeStartable(object):
-    def __init__(self, name, dependencies=None):
-        dependencies = [] if dependencies is None else dependencies
-        self.app = create_application(name, dependencies)
-
-    def start(self, host='127.0.0.1', port=5000):
-        # With Multi-Threading Apps, YOU CANNOT USE DEBUG! Though you can sub-thread.
-        th = Thread(target=lambda: self.app.run(host, port, debug=False, threaded=True))
-        th.start()
 
 
 def main():
