2783e9ae6a7ff9972f139b599aa485d449d68789
Robert Sebastian Herlim <ngiooong@yahoo.com>
2020-06-13 22:14:06 +0900

    remove dependency from poc

--- bootstrap.py
+++ bootstrap.py
@@ -1,42 +1,88 @@
-from abc import ABC, abstractmethod
-from node import INode, ResponseLevel
-import sys
-sys.path.append("poc/")
-from poc.poc import POCNode
-from poc.bootstrap_flask import POCNodeStartable, PORT_APP
-import random
+from concurrent.futures import _base
+from concurrent.futures.thread import ThreadPoolExecutor
+from threading import Thread
+from time import sleep
+from typing import Iterable
 
-# TODO: if it is possible, we might want to be able to automatically start the microservice environment
-# This is just an example to bootstrap the microservice
+from flask import Flask, Response, current_app, request
+from requests import get as http_get
+
+from node import ResponseLevel, inverse_response_level, SimpleNode
+
+PORT_APP = dict()
 
 
-class IStartable(ABC):
-    @abstractmethod
-    def start(self):
-        pass
+def create_application(app_name: str, dependencies=None):
+    dependencies = [] if dependencies is None else dependencies
+    app_name = app_name.replace(',', '_')
 
-# TODO: IMPORTANT! THIS CODE DESIGN IS TERRIBLY WRONG. Please refer to the PoC in poc directory.
-class SimpleNodeStartable(INode, IStartable):
-    dependencies = []
+    app = Flask(app_name)
+    app.__response_level = ResponseLevel.NORMAL
+    app.__dependencies = dependencies
+    app.__executor = ThreadPoolExecutor(max_workers=16)
 
-    def __str__(self):
-        pass
+    # check dependencies must run asynchronously
+    def check_dependencies(visited: str, dependencies: Iterable[str], executor: _base.Executor) -> bool:
+        visited_set = set(visited.split(','))
+        filtered_dep = filter(lambda x: PORT_APP[int(x.split(':')[1])] not in visited_set, dependencies)
 
-    def ping(self):
-        pass
+        futures = []
+        for target in filtered_dep:
+            get_url = f'http://{target}/?from={visited}'
+            future = executor.submit(lambda: http_get(get_url))
+            futures.append((get_url, future))
 
-    def set_response_level(self, response_level: ResponseLevel):
-        pass
+        for (get_url, future) in futures:
+            result = future.result()
+            is_ok = result.status_code == 200
+            if not is_ok:
+                return False
 
-    def get_name(self):
-        pass
+        return True
 
-    def set_dependencies(self, node):
-        self.dependencies = ['{}:{}'.format(x.ip_address, x.port) for x in node]
+    @app.route('/', methods=['GET'])
+    @app.route('/ping', methods=['GET'])
+    def ping():
+        visited = request.args.get('from')
+        updated_visited = current_app.name + ('' if visited is None else (',' + visited))
 
-    def start(self):
-        # TODO: when called, this method will start the webserver on specified ip_address & port
-        pass
+        response_level = current_app.__response_level
+        if response_level == ResponseLevel.NORMAL:
+            dependencies_ok = check_dependencies(updated_visited, current_app.__dependencies, current_app.__executor)
+            message, code = ('OK', 200) if dependencies_ok else ('UNHEALTHY', 500)
+            return Response(message, code)
+
+        elif response_level == ResponseLevel.TERMINATED:
+            return Response('UNHEALTHY', 500)
+
+        else:
+            sleep(5.0)
+            dependencies_ok = check_dependencies(updated_visited, current_app.__dependencies, current_app.__executor)
+            message, code = ('OK', 200) if dependencies_ok else ('UNHEALTHY', 500)
+            return Response(message, code)
+
+    @app.route('/response/<level>', methods=['GET'])
+    def set_response_level(level):
+        try:
+            level = int(level)
+            response_level = inverse_response_level(level)
+            current_app.__response_level = response_level
+            return 'OK'
+        except ValueError as e:
+            return str(e)
+
+    return app
+
+
+class SimpleNodeStartable(object):
+    def __init__(self, name, dependencies=None):
+        dependencies = [] if dependencies is None else dependencies
+        self.app = create_application(name, dependencies)
+
+    def start(self, host='127.0.0.1', port=5000):
+        # With Multi-Threading Apps, YOU CANNOT USE DEBUG! Though you can sub-thread.
+        th = Thread(target=lambda: self.app.run(host, port, debug=False, threaded=True))
+        th.start()
 
 
 def bootstrap(graph:{}):
@@ -50,6 +96,9 @@ def bootstrap(graph:{}):
     @return:
         Startable Node list
     """
+    # for (port, name) in zip(ports, names):
+    #     PORT_APP[port] = name
+
     host = '127.0.0.1'
     name_and_address = []
     dependencies = dict()
@@ -64,9 +113,9 @@ def bootstrap(graph:{}):
                 continue
             dependencies[node_name].append(host+':'+str(5000+int(dependent)))
         if len(dependencies[node_name]) == 0:
-            POCNodeStartable(node_name).start(port=port)
+            SimpleNodeStartable(node_name).start(port=port)
         else:
-            POCNodeStartable(node_name, dependencies[node_name]).start(port=port)
+            SimpleNodeStartable(node_name, dependencies[node_name]).start(port=port)
         PORT_APP[port] = node_name
 
     # create Node
@@ -75,24 +124,12 @@ def bootstrap(graph:{}):
         name = ele[0]
         port = ele[1].split(":")[1]
         port = int(port)
-        nodes.append(POCNode(name, host, port))
+        nodes.append(SimpleNode(name, host, port))
 
     # return [node_list]
     return nodes
 
 
-def construct_random_graph():
-    N = random.randint(1,1000)
-    graph = {}
-    for i in range(N):
-        dependencies = []
-        for j in range(N):
-            if j == i:
-                continue
-            if round(random.random()) == 0:
-                dependencies.append(j)
-        graph[i] = dependencies
-    return graph
 if __name__ == '__main__':
     graph = {1:{2,3},2:{4},3:{},4:{}}
     nodes = bootstrap(graph)
