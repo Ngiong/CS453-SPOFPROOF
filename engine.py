cbb7fe99ab23d56ddc33a5f16c633ebb81abec0c
Robert Sebastian Herlim <ngiooong@yahoo.com>
2020-06-13 23:41:09 +0900

    refactor engine

--- engine.py
+++ engine.py
@@ -41,67 +41,26 @@ class SPOFProofEngine:
     def print_dependency(self):
         print(self.dependency_found)
 
+
 def main():
-    # Adding the boostrap part
-    app1 = POCNodeStartable('app1')
-    app1.start(port=5000)
-
-    app2 = POCNodeStartable('app2', ['127.0.0.1:5000'])
-    app2.start(port=5001)
-
-    app3 = POCNodeStartable('app3', ['127.0.0.1:5001'])
-    app3.start(port=5002)
-
-    app4 = POCNodeStartable('app4', ['127.0.0.1:5001'])
-    app4.start(port=5003)
-
-    app5 = POCNodeStartable('app5', ['127.0.0.1:5000', '127.0.0.1:5002'])
-    app5.start(port=5004)
-
-    names = ['app1', 'app2', 'app3', 'app4', 'app5']
-    ports = [5000, 5001, 5002, 5003, 5004]
-
-    for (port, name) in zip(ports, names):
-        PORT_APP[port] = name
-
-    #----------------------------------------------
-    # Test if the NodeStartable is running correctly
-    # node1 = POCNode('app1', '127.0.0.1', 5000)
-    # assert node1.ping()
-
-    # assert node1.set_response_level(ResponseLevel.TERMINATED)
-    # assert not node1.ping()
-
-    # assert node1.set_response_level(ResponseLevel.SLOW)
-    # assert node1.ping()
-
-    # # Check dependency
-    # node2 = POCNode('app2', '127.0.0.1', 5001)
-    # node1.set_response_level(ResponseLevel.NORMAL)
-    # assert node2.ping()
-
-    # node1.set_response_level(ResponseLevel.TERMINATED)
-    # assert not node2.ping()
-    # print("Test success")
-    # node1.resurrect()
-
     # Initialize node for test
-    node1 = POCNode('app1', '127.0.0.1', 5000)
-    node2 = POCNode('app2', '127.0.0.1', 5001)
-    node3 = POCNode('app3', '127.0.0.1', 5002)
-    node4 = POCNode('app4', '127.0.0.1', 5003)
-    node5 = POCNode('app5', '127.0.0.1', 5004)
+    node1 = POCNode('app1', '127.0.0.1', 5001)
+    node2 = POCNode('app2', '127.0.0.1', 5002)
+    node3 = POCNode('app3', '127.0.0.1', 5003)
+    node4 = POCNode('app4', '127.0.0.1', 5004)
 
-    nodes = [node1, node2, node3, node4, node5]
+    nodes = [node1, node2, node3, node4]
 
+    # Nodes OK?
     for node in nodes:
         assert node.ping()
 
     # Test the engine
     engine = SPOFProofEngine()
     engine.set_nodes(nodes)
-    engine.run_test()
+    engine.run_repeated_test()
     engine.print_dependency()
+
 
 if __name__ == '__main__':
     main()
