381a117afd0ef3d46e64446633a41bac39752df7
David9902 <david.rombach@outlook.de>
2020-06-14 12:46:35 +0200

    function integration

--- graph.py
+++ graph.py
@@ -1,7 +1,5 @@
 import pydot
 import time
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 
 # create graph object
@@ -9,28 +7,31 @@ class Node_Graph():
     name = ''
     G = None
     target_node = ""
-    current_edge_test = None
+    source_node = ""
+    colored_edges_dest = []
 
     def __init__(self):
         self.G = pydot.Dot(graph_type="digraph")
         self.G.set('splines', True)
-        self.G.set_size("9, 15\!")
-        self.G.set_dpi(100)
-
-        self.G.write_png("./__init.png")
-        img = mpimg.imread('./__init.png')
-        # X = [[1]]# sample 2D array
-        self.figure = plt.imshow(img, aspect=1)  # , aspect='auto'
-        # plt.axis("scaled")
-        # plt.axis("off")
-        plt.ion()
-        # self.gui_thread = threading.Thread(target=self.show_matplot)
+        
+    def set_source_node(self, node_name):
+        # get current source_node and reset node and edge color
+        if self.source_node != "":
+            current = self.G.get_node(self.source_node)
+            current[0].set_style("None")
+            self.edge_color_normalization(self.source_node)
+        graph_node = self.G.get_node(node_name)
+        graph_node[0].set_style("filled")
+        graph_node[0].set_fillcolor("blue")
+        self.source_node = node_name
 
     def set_target_node(self, node_name):
         # get current target_node and reset color
         if self.target_node != "":
             current = self.G.get_node(self.target_node)
-            current[0].set_style("None")
+            # only reset color if it is not the source node
+            if current[0].get_fillcolor() != "blue":
+                current[0].set_style("None")
 
         graph_node = self.G.get_node(node_name)
         graph_node[0].set_style("filled")
@@ -42,6 +43,8 @@ class Node_Graph():
         edge = self.G.get_edge(src, dest)
         if len(edge) > 0:
             edge[0].set_color(color)
+            if color != "black":
+                self.colored_edges_dest.append(dest)
 
     def add_node(self, node_name, pos=None):
         if pos is None:
@@ -53,7 +56,7 @@ class Node_Graph():
             self.G.add_node(node)
 
     def add_edge(self, node1, node2):
-        edge = pydot.Edge(node1, node2)  # , color="blue"
+        edge = pydot.Edge(node1, node2)
         self.G.add_edge(edge)
 
     def remove_edge(self, node1, node2):
@@ -71,25 +74,21 @@ class Node_Graph():
         else:
             self.G.write(path, prog='neato', format='png')
 
-    # def show_matplot(self):
-    #     t = threading.currentThread()
-    #     while getattr(t, "do_run", True):
-    #         # print("called")
-    #         img = mpimg.imread('./__pic.png')
-    #         # self.figure.set_data(img)
-    #         plt.figure(img)
-    #         plt.show()
-    #         plt.pause(0.001)
-    #         # print("plotted")
-    #     # print("Stopping as you wish.")
-    #
-    # def gui_thread_start(self):
-    #     self.gui_thread.start()
-    #     # threading.Thread(target=self.show_matplot).start()
-    #
-    # def gui_thread_stop(self):
-    #     self.gui_thread.do_run = False
-    #     self.gui_thread.join()
+    # getting called, when source node is changed
+    def edge_color_normalization(self, src):
+        for node in self.colored_edges_dest:
+            self.set_edge_color(src,node,"black")
+        self.colored_edges_dest = []
+
+    # reset colors of source and target node
+    def graph_build_finished(self):
+        if self.source_node != "":
+            self.edge_color_normalization(self.source_node)
+            current = self.G.get_node(self.source_node)
+            current[0].set_style("None")
+        if self.target_node != "":
+            current = self.G.get_node(self.target_node)
+            current[0].set_style("None")
 
 
 class GraphVisualizer:
