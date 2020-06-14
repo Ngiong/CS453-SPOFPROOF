38903dbbe406c720506653bc36a70ddb66a454c1
David9902 <david.rombach@outlook.de>
2020-06-14 07:05:55 +0200

    code cleanup, store pictures

--- graph.py
+++ graph.py
@@ -1,13 +1,7 @@
 import pydot
 import os
-import threading
 import time
 from poc.poc import POCNode
-
-""" from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg """
-
 import webbrowser as web
 
 #create graph object
@@ -21,25 +15,12 @@ class Node_Graph():
     def __init__(self, name ): 
         self.name = name
         self.G = pydot.Dot(graph_type="digraph")
-        
-        """ self.G.set_size("9,15\!")
-        self.G.set_dpi(100)
-        
-        self.G.write_png("./init.png")
-        img = mpimg.imread('./init.png')
-        #X = [[1]]# sample 2D array
-        self.figure= plt.imshow(img, aspect=1)  #, aspect='auto'
-        #plt.axis("scaled")
-        #plt.axis("off")
-        plt.ion()
-        self.gui_thread = threading.Thread(target=self.show_matplot) """
-
 
     def add_node(self, node):
         self.G.add_node(pydot.Node(node.name))
 
     def add_edge(self, node1, node2):
-        edge = pydot.Edge(node1.name, node2.name)  #, color="blue"
+        edge = pydot.Edge(node1.name, node2.name)
         self.G.add_edge(edge)
     
     def del_edge(self, node1, node2):
@@ -52,30 +33,19 @@ class Node_Graph():
 
     def set_source_node(self, node):
         # get current source_node and reset node and edge color
-        """ print("change source node")
-        print(self.source_node)
-        print(" to ", node) """
         if self.source_node != "":
             current = self.G.get_node(self.source_node.name)
-            #print("current:", current, "style:", current[0].get_fillcolor())
-
             current[0].set_style("None")
-            #print("DOO NORMALIZATION")
             self.edge_color_normalization(self.source_node)
-
-
         graph_node = self.G.get_node(node.name)
         graph_node[0].set_style("filled")
         graph_node[0].set_fillcolor("blue")
-
         self.source_node = node
 
     def set_target_node(self, node):
         # get current target_node and reset color
         if self.target_node != "":
             current = self.G.get_node(self.target_node)
-            #print(current)
-            #print("set target node current: ", current[0].get_fillcolor())
             # only reset color if it is not the source node
             if current[0].get_fillcolor() != "blue":
                 current[0].set_style("None")
@@ -83,35 +53,18 @@ class Node_Graph():
         graph_node = self.G.get_node(node.name)
         graph_node[0].set_style("filled")
         graph_node[0].set_fillcolor("red")
-
         self.target_node = node.name
 
     def set_edge_color(self, src, dest, color):
-        #print("all edges")
-        """ for e in self.G.get_edges():
-            print(e)
-        print("-----") """
-        #print("set edge color: ", src, dest, color)
         edge = self.G.get_edge(src.name, dest.name)
-
-
-        #print(edge)
         if len(edge) > 0:
-            #print("change edge color")
             edge[0].set_color(color)
-            #print(edge[0])
             if color != "black":
-                #print("added to colored_edges", dest)
                 self.colored_edges_dest.append(dest)
 
     #getting called, when source node is changed
     def edge_color_normalization(self, src):
-        """ print("color normalization")
-        print("src", type(src)) """
         for node in self.colored_edges_dest:
-            #print("dest", type(node))
-
-            #print(src, "-->",  node)
             self.set_edge_color(src,node,"black")
         self.colored_edges_dest = []
 
@@ -119,33 +72,10 @@ class Node_Graph():
         if self.source_node != "":
             self.edge_color_normalization(self.source_node)
 
-    """ def show_matplot(self):
-        t = threading.currentThread()
-        while getattr(t, "do_run", True):
-            #print("called")
-            img = mpimg.imread('./pic.png')
-            #self.figure.set_data(img)
-            plt.figure(img)
-            plt.show()
-            plt.pause(0.001)
-            #print("plotted")     
-        #print("Stopping as you wish.")   """ 
-     
-
-
-    """ def gui_thread_start(self):
-        self.gui_thread.start()
-        #threading.Thread(target=self.show_matplot).start()
-
-    def gui_thread_stop(self):
-        self.gui_thread.do_run = False
-        self.gui_thread.join() """
-
 
 def testing(): 
     delay = 0
     web.open(os.path.dirname(os.path.realpath(__file__))+"/spofproof.html")
-    print("Hello world")
     graph = Node_Graph("testgraph")
     node1 = POCNode('app1', '127.0.0.1', 5000)
     node2 = POCNode('app2', '127.0.0.1', 5001)
@@ -163,7 +93,6 @@ def testing():
     graph.set_target_node(node2)
     graph.add_edge(node1,node2)
     graph.save_graph("./pic1.png")
-    #graph.gui_thread_start()
     time.sleep(delay)
 
     graph.del_edge(node1, node2)
@@ -197,7 +126,6 @@ def testing():
     graph.set_target_node(node1)
     graph.add_edge(node2,node1)
     graph.save_graph("./pic7.png")
-    #graph.gui_thread_start()
     time.sleep(delay)
 
     graph.set_edge_color(node2,node1,"green")
@@ -230,7 +158,6 @@ def testing():
     graph.set_target_node(node1)
     graph.add_edge(node3,node1)
     graph.save_graph("./pic13.png")
-    #graph.gui_thread_start()
     time.sleep(delay)
 
     graph.del_edge(node3, node1)
@@ -264,7 +191,6 @@ def testing():
     graph.set_target_node(node1)
     graph.add_edge(node4,node1)
     graph.save_graph("./pic19.png")
-    #graph.gui_thread_start()
     time.sleep(delay)
 
     graph.set_edge_color(node4,node1,"green")
@@ -291,16 +217,10 @@ def testing():
     graph.save_graph("./pic24.png")
     time.sleep(delay)
 
-
+    # finish raph building
     graph.graph_build_finished()
     graph.save_graph("./pic25.png")
 
 
-
-
-
-
-
-    #graph.gui_thread_stop()
 if __name__ == '__main__':
     testing()
